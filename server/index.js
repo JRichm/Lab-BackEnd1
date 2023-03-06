7af3c9c267fd6b8e85830003e2679f9d11692d2e
JRichm <jblackenmusic@gmail.com>
2023-03-06 15:50:21 -0700

    finished lab

--- server/index.js
+++ server/index.js
@@ -2,20 +2,21 @@ const express = require('express');
 const cors = require('cors');
 
 const app = express();
+
 app.use(express.json());
 app.use(cors());
 
 port = 4000;
 
-app.listen(port, () => console.log(`Server started on port ${port}`));
-
 app.get("/api/users", (req, res) => {
-    let friends = ["Nitin", "Eric", "Jeddy", "Cameron", "Riley"];
-    res.status(200).send(friends);
-  });
+  let friends = ["Nitin", "Eric", "Jeddy", "Cameron", "Riley"];
+  res.status(200).send(friends);
+});
 
-  app.get("/weather/:temperature", (req, res) => {
-    const { temperature } = req.params;
-    const phrase = `<h3>It was ${temperature} today</h3>`;
-    res.status(200).send(phrase);
-  });
+app.get("/weather/:temperature", (req, res) => {
+  const { temperature } = req.params;
+  const phrase = `<h3>It was ${temperature} today</h3>`;
+  res.status(200).send(phrase);
+});
+
+app.listen(port, () => console.log(`Server started on port ${port}`)); 
